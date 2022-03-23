304504498bb019ff7c660c8fc9654591e1f5da8d
caleb-diehl-ibigroup <100301766+caleb-diehl-ibigroup@users.noreply.github.com>
2022-03-23 16:33:01 -0700

    Delete gtfs-frequency-mapping-tool-main/gtfs-frequency-mapping-tool-main directory

--- gtfs-frequency-mapping-tool-main/gtfs-frequency-mapping-tool-main/config.py
+++
@@ -1,9 +0,0 @@
-settings = {
-    'Feed': 'bramptonTransit.zip',
-    'Direction': 'Inbound',
-    'Day Type': 'weekday',
-    'Network Setting': 'Network Wide',
-    'Routes': [],
-    'Time Interval': [6, 9, "No"],
-    'File Name': 'brampTest'
-}
