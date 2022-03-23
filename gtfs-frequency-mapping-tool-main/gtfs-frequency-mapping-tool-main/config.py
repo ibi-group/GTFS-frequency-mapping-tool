f771294d302ce36cf1b6de032358fd8381a85df3
caleb-diehl-ibigroup <100301766+caleb-diehl-ibigroup@users.noreply.github.com>
2022-03-23 16:32:20 -0700

    Add files via upload

---
+++ gtfs-frequency-mapping-tool-main/gtfs-frequency-mapping-tool-main/config.py
@@ -0,0 +1,9 @@
+settings = {
+    'Feed': 'bramptonTransit.zip',
+    'Direction': 'Inbound',
+    'Day Type': 'weekday',
+    'Network Setting': 'Network Wide',
+    'Routes': [],
+    'Time Interval': [6, 9, "No"],
+    'File Name': 'brampTest'
+}
